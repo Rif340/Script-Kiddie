04dde2541342578f19d09f267d9b274ee895aee3
To Be Human <67031062+Rif340@users.noreply.github.com>
2024-02-28 23:24:00 +0700

    Add files via upload

---
+++ CallOffDutyMobile.lua
@@ -0,0 +1,521 @@
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+gg.setVisible(false)
+gg.toast("■□□□□□□□□□")
+gg.toast("■■□□□□□□□□")
+gg.toast("■■■□□□□□□□")
+gg.toast("■■■■□□□□□□")
+gg.toast("■■■■■□□□□□")
+gg.toast("■■■■■■□□□□")
+gg.toast("■■■■■■■□□□")
+gg.toast("■■■■■■■■□□")
+gg.toast("■■■■■■■■■□")
+gg.toast("■■■■■■■■■■")
+gg.setVisible(true)
+gg.alert("Welcome  ")
+--[[ return: 1 ]]
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'MENU BYPASS'
+gg.multiChoice({
+[1] = '💥FIX CRASH  BYPASS LOGO💥',
+[2] = ' باگ استفاده نکنید',
+[3] = '🔱BACK🔱',
+}, nil, "VIP SCRIPT")
+--'💥FIX CRASH  BYPASS LOGO💥'
+gg.toast("Welcom")
+gg.alert("AMIR.KL")
+--[[ return: 1 ]]
+gg.alert("ØªØ§ÛŒÛŒØ¯ Ø´Ø¯")
+--[[ return: 1 ]]
+gg.choice({
+[1] = 'MENU BYPASS',
+}, nil, "AMIR.KL")
+--[[ return: nil ]]
+gg.multiChoice({
+[1] = 'ðŸ’¥FIX CRASH  BYPASS LOGOðŸ’¥',
+}, nil, "AMIR.KL")
+--[[ return: {
+} ]]
+so = gg.getRangesList('libil2cpp.so')[1].start
+py = 0x57
+setvalue(so + py, 4, 0)
+gg.alert("on")
+--[[ return: 1 ]]
+so = gg.getRangesList('libanogs.so')[1].start
+py = 0x718A8
+setvalue(so + py, 4, 0)
+gg.alert("on")
+--[[ return: 1 ]]
+gg.setVisible(false)
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'ðŸ’¥FIX CRASH  BYPASS LOGOðŸ’¥'
+gg.multiChoice({
+[1] = '💥FIX CRASH  BYPASS LOGO💥',
+[2] = ' باگ استفاده نکنید',
+[3] = '🔱BACK🔱',
+}, nil, "VIP SCRIPT")
+--' باگ استفاده نکنید'
+gg.setVisible(false)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.alert("ᴀɴᴛɪᴄʜᴇᴀᴛ ʀᴇꜱᴜʟᴛ ➪ 0\nꜱᴛᴀᴛᴜꜱ ➪ امن✔︎\nʀᴇʟᴏɢ ➪ ɴᴏ ")
+--[[ return: 0 ]]
+gg.clearResults()
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'🔴Red Line🔴(بدون بن)'
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("4575657222490554368;1065353216:9", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("4575657222490554368;1065353216", gg.TYPE_QWORD)
+gg.getResults(9999)
+gg.editAll("4575657222520438784;1133903872", gg.TYPE_QWORD)
+gg.clearResults()
+gg.toast("ACTIVATED")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--' Magic Bullet(head)(احتمال بن)'
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.searchNumber("0.1439999938", gg.TYPE_FLOAT)
+gg.refineNumber("0.1439999938", gg.TYPE_FLOAT)
+gg.getResults(9999)
+gg.editAll("1.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.searchNumber("0.1439999938", gg.TYPE_FLOAT)
+gg.refineNumber("0.1439999938", gg.TYPE_FLOAT)
+gg.getResults(9999)
+gg.editAll("1.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Magic Head Activated")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'💠AimLock💠 (احتمال بن)'
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.searchNumber("1.40129846e-45;0.44999998808:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("0.44999998808", gg.TYPE_FLOAT)
+gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("4", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Aim bot Activated")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'🏃Speed Hack🏃(احتمال بن)'
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.prompt({
+[1] = '🏃??sᴇᴛ sᴘᴇᴇᴅ ᴛᴏ🏃🏻‍♀[5;20]',
+[2] = 'ɴᴏ ʟᴀɢ(ɢᴏᴏᴅ ғᴏʀ ʙʀ)',
+}, {
+}, {
+[1] = 'number',
+[2] = 'checkbox',
+})
+--[[ return: {
+[1] = '5',
+[2] = true,
+} ]]
+gg.setVisible(false)
+gg.toast("Starting SpeedHack: 5")
+gg.setVisible(false)
+gg.toast("Starting SpeedHack: 5")
+gg.setVisible(false)
+gg.toast("Starting SpeedHack: 5")
+gg.setVisible(false)
+gg.toast("Starting SpeedHack: 5")
+gg.setVisible(false)
+gg.toast("Starting SpeedHack: 5")
+gg.searchNumber("4.28000020981;3.20000004768", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(999)
+gg.editAll("5", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Active")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)'
+gg.alert("Choose Mode ", "MP", "BR")
+--[[ return: 2 ]]
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1,065,353,216;1,069,547,520;1,050,253,722:9", gg.TYPE_DWORD)
+gg.refineNumber("1,065,353,216", gg.TYPE_DWORD)
+gg.getResults(9999)
+gg.editAll("1,082,130,432", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Active")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)'
+gg.alert("Choose Mode ", "MP", "BR")
+--[[ return: 1 ]]
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("7.5;0.8", gg.TYPE_FLOAT)
+gg.refineNumber("0.8", gg.TYPE_FLOAT)
+gg.getResults(9999)
+gg.editAll("4", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Active")
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
+--'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)'
+gg.multiChoice({
+[ 1] = 'ᴅʟϙ33 ʟᴏᴛᴜs ғʟᴀᴍᴇs',
+[ 2] = 'ᴀᴋ47 ʀᴀᴅɪᴀɴᴄᴇ',
+[ 3] = 'ᴋɪʟᴏ 141 ᴅᴇᴍᴏɴsᴏɴɢ',
+[ 4] = 'ᴄʙʀ ᴀᴍᴏᴇʙᴏᴀ',
+[ 5] = 'ᴍx9 ʜᴇᴀʀᴛʟᴇss',
+[ 6] = 'ᴍx9 ᴇxᴏsᴛᴀᴛɪᴄ',
+[ 7] = 'ᴍᴀᴄ 10 ʙᴀɴᴀɴᴀ',
+[ 8] = 'ᴀᴋ47 ᴘᴜᴍᴘᴋɪɴ ʜᴇᴀᴅ',
+[ 9] = 'ɪᴄʀ-1 ғᴏʀᴄᴇᴅ ʟᴀᴜɢʜᴛᴇʀ',
+[10] = 'ᴘᴅᴡ ʙᴀᴛᴛᴀʟɪᴏɴ',
+[11] = 'ʟᴏᴄᴜs ᴇʟᴇᴄᴛʀᴏɴ',
+[12] = 'by 15🥵🥵',
+[13] = '𝙗𝙖𝙘𝙠',
+}, nil, "Sᴄʀɪᴘᴛ Bʏ Sᴋɪɴ Hᴀᴄᴋ 1.6.39")
+--[[ return: {
+[ 1] = true,
+[ 2] = true,
+[ 3] = true,
+[ 4] = true,
+[ 5] = true,
+[ 6] = true,
+[ 7] = true,
+[ 8] = true,
+[ 9] = true,
+[10] = true,
+[11] = true,
+[12] = true,
+} ]]
+gg.toast("amir")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10207001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/4 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10207001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/4 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10207201, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/4 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10207001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("4/4 Failed")
+gg.clearResults()
+gg.toast("Success")
+gg.alert("DLQ Mythic Active")
+--[[ return: 1 ]]
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("2;30;200000029;200000233;200002161::21", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("100524;100233;66734;66740;66737", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("200,002,161;10107001;1,057,803,469::31", gg.TYPE_DWORD)
+gg.refineNumber("200002161", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("66737", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("ᴀᴋ47 ʀᴀᴅɪᴀɴᴄᴇ ᴏɴ")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("2;100,149;9,187;9205;9196::21", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("100511;100155;9470;9488;9479", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9,196;10,124,001;1,057,803,469", gg.TYPE_DWORD)
+gg.refineNumber("9196", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("9479", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("ᴋɪʟᴏ ᴅᴇᴍᴏɴsᴏɴɢ ᴏɴ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(6808, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/2 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(6816, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/2 Failed")
+gg.clearResults()
+gg.toast("ᴄʙʀ4 ᴀᴍᴏᴇʙᴀ")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("15;10902;602000736;602000737;200011017::21", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("100424;10900;602000732;602000733;200011015", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("200,011,017;10419001;1,057,803,469::31", gg.TYPE_DWORD)
+gg.refineNumber("200,011,017", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("200011015", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("ᴍx9 ʜᴇᴀʀᴛʟᴇss ᴏɴ")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("15;10902;602000736;602000737;200011017::21", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("100536;100265;67831;67849;67840", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("200,011,017;10419001;1,057,803,469::31", gg.TYPE_DWORD)
+gg.refineNumber("200,011,017", gg.TYPE_DWORD)
+gg.getResults(10000)
+gg.editAll("67840", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("ᴍx9 ᴇxᴏsᴛᴀᴛɪᴄ ᴏɴ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10422001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10422001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10403001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/3 Failed")
+gg.clearResults()
+gg.toast("ᴍᴀᴄ 10 ʙᴀɴᴀɴᴀ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10107001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10107001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10109001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/3 Failed")
+gg.clearResults()
+gg.toast("ᴀᴋ47 ᴘᴜᴍᴘᴋɪɴ ʜᴇᴀᴅ ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10112001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10112001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10109001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/3 Failed")
+gg.clearResults()
+gg.toast("ɪᴄʀ 1 ғᴏʀᴄᴇᴅ ʟᴀᴜɢʜᴛᴇʀ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10406001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10406001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10403001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/3 Failed")
+gg.clearResults()
+gg.toast("ᴘᴅᴡ ʙᴀᴛᴛᴀʟɪᴏɴ ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10208001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("1/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10208001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/3 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10207001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/3 Failed")
+gg.clearResults()
+gg.toast("ʟᴏᴄᴜs ᴇʟᴇᴄᴛʀᴏɴ")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10503001, gg.TYPE_DWORD)
+gg.getResultsCount()
+Fuck You Copy paster Failed
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10503001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("2/1 Failed")
+gg.clearResults()
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(10503001, gg.TYPE_DWORD)
+gg.getResultsCount()
+gg.toast("3/1 Failed")
+gg.clearResults()
+gg.alert("Active  by15")
+--[[ return: 1 ]]
+gg.setVisible(false)
+gg.toast(" Put Your Name Here ")
+gg.multiChoice({
+[1] = 'MENU BYPASS',
+[2] = '🔴Red Line🔴(بدون بن)',
+[3] = ' Magic Bullet(head)(احتمال بن)',
+[4] = '💠AimLock💠 (احتمال بن)',
+[5] = '🏃Speed Hack🏃(احتمال بن)',
+[6] = '🔥𝙝𝙞𝙜𝙝 𝙟𝙪𝙢𝙥🔥(احتمال بن)',
+[7] = 'Mᴇɴᴜ Sᴋɪɴ Hᴀᴄᴋ(احتمال کرش)',
+[8] = ' [ EXIT ]',
+}, nil, " Put Your Channel Here Or Sth Else ")
